a39e5a9c5d20d450a83d2348f6ccae153d47f191
Michael T Jones <michael.jones@gmail.com>
2018-01-21 21:38:14 -0800

    Update pcg64.go

--- pcg64.go
+++ pcg64.go
@@ -5,6 +5,17 @@ package pcg
 // Paper and details at http://www.pcg-random.org
 // Ported to Go by Michael Jones <michael.jones@gmail.com>
 
+// Copyright 2018 Michael T. Jones
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance 
+// with the License. You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed 
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for 
+// the specific language governing permissions and limitations under the License.
+
 type PCG64 struct {
 	lo *PCG32
 	hi *PCG32
